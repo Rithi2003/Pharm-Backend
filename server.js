8f2e7cf4bf3ed6b62723f39b867a1cb79345fc03
Rithi2003 <122975652+Rithi2003@users.noreply.github.com>
2023-11-06 12:56:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const connectDatabase = require("./config/database");
 
 const dotenv = require("dotenv");
 const cloudinary = require('cloudinary')
-const cors=require("cors");
 
-app.use(cors());
 
 // Handle Uncaught exceptions
 process.on("uncaughtException", (err) => {
